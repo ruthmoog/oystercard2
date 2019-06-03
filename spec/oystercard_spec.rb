e95811df15ebd5a9c9eb162441e26e7ac54b6ff9
Ruth Baker <ruthmoog@gmail.com>
2019-06-03 17:14:21 +0100

    Allow top_up balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,14 @@
 require "oystercard"
 
 RSpec.describe Oystercard do
-   it 'has an initial balance of 0' do
-     expect(subject.balance).to eq(0)
-   end
+  it 'has an initial balance of 0' do
+    expect(subject.balance).to eq(0)
+  end
 
+  it 'receives a top up amount of 5' do
+    subject.top_up(5)
+
+    expect(subject.balance).to eq(5)
+  end
 
 end
